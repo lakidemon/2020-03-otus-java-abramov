1e4cd4c348fa77f9f5abe9331171ec91a973b768
lakidemon <forregistrationsmail@mail.ru>
2020-06-01 00:38:46 +0700

    [HW08] Tests

--- buildSrc/src/main/java/Deps.java
+++ buildSrc/src/main/java/Deps.java
@@ -2,4 +2,5 @@ public class Deps {
     public static final String guava = "28.2-jre";
     public static final String junit = "5.6.2";
     public static final String gfJson = "1.1.4";
+    public static final String gson = "2.8.6";
 }
---
+++ hw08-json/src/test/java/ru/otus/json/OtusonTest.java
@@ -0,0 +1,58 @@
+package ru.otus.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Сериализатор")
+public class OtusonTest {
+    private static Otuson otuson;
+    private static Gson gson;
+
+    @BeforeAll
+    static void setUp() {
+        otuson = Otuson.create();
+        gson = new GsonBuilder().serializeNulls().create();
+    }
+
+    @Test
+    @DisplayName("должен сериализовать примитивные типы и строки")
+    void shouldSerializePrimitives() {
+        assertEquals("\"String\"", otuson.toJson("String"));
+        assertEquals("100", otuson.toJson(100));
+        assertEquals("100", otuson.toJson((byte) 100));
+        assertEquals("100", otuson.toJson((short) 100));
+        assertEquals("100.1", otuson.toJson(100.1));
+        assertEquals("10000000000", otuson.toJson(10000000000L));
+        assertEquals("true", otuson.toJson(true));
+        assertEquals("false", otuson.toJson(false));
+    }
+
+    @Test
+    @DisplayName("должен сериализовать коллекции и массивы")
+    void shouldSerializeCollectionsAndArrays() {
+        var list = Arrays.asList(100, 500, 100500);
+        var expecting = "[100,500,100500]";
+        assertEquals(expecting, otuson.toJson(list));
+        assertEquals(expecting, otuson.toJson(list.toArray(Integer[]::new)));
+    }
+
+    @Test
+    @DisplayName("должен сериализовать сложные объекты")
+    void shouldSerializeComplexObjects() {
+        var object = new SomeObject();
+        var reference = gson.toJsonTree(object);
+        var json = otuson.toJson(object);
+        var parsed = gson.fromJson(json, JsonObject.class);
+        var fromJson = gson.fromJson(json, SomeObject.class);
+        assertEquals(reference, parsed);
+        assertEquals(object, fromJson);
+    }
+}
