8440892c3856348740922f30795193a577daa3d2
lakidemon <forregistrationsmail@mail.ru>
2020-06-06 00:46:51 +0700

    [HW09] Init commit [2]

--- buildSrc/src/main/java/Deps.java
+++ buildSrc/src/main/java/Deps.java
@@ -3,4 +3,6 @@ public class Deps {
     public static final String junit = "5.6.2";
     public static final String gfJson = "1.1.4";
     public static final String gson = "2.8.6";
+    public static final String h2 = "1.4.200";
+    public static final String logback = "1.2.3";
 }
