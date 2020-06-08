c183a8944f43c3fe32087cc09a1f5adff998a709
lakidemon <forregistrationsmail@mail.ru>
2020-06-08 23:54:39 +0700

    [HW09] Mutable User name

--- hw09-orm/src/main/java/ru/otus/core/model/User.java
+++ hw09-orm/src/main/java/ru/otus/core/model/User.java
@@ -1,12 +1,13 @@
 package ru.otus.core.model;
 
 import lombok.Data;
+import lombok.NonNull;
 import ru.otus.jdbc.mapper.Id;
 
 @Data
 public class User {
     @Id
     private final long id;
-    private final String name;
+    private @NonNull String name;
     private final int age;
 }
