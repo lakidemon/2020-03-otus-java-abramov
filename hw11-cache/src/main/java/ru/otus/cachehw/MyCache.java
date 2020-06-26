07acf55a48353ef15ecad5b651e6dd34d31008e2
lakidemon <forregistrationsmail@mail.ru>
2020-06-27 03:00:15 +0700

    [HW11] Soft listeners

--- hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -2,6 +2,7 @@ package ru.otus.cachehw;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +15,7 @@ import java.util.WeakHashMap;
 @Slf4j
 public class MyCache<K, V> implements HwCache<K, V> {
     private WeakHashMap<K, V> map = new WeakHashMap<>();
-    private List<HwListener<K, V>> listeners = new ArrayList<>();
+    private List<SoftReference<HwListener<K, V>>> listeners = new ArrayList<>();
 
     @Override
     public void put(K key, V value) {
@@ -34,16 +35,20 @@ public class MyCache<K, V> implements HwCache<K, V> {
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        listeners.add(listener);
+        listeners.add(new SoftReference<>(listener));
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        listeners.remove(listener);
+        listeners.removeIf(ref -> listener == ref.get());
     }
 
     private void notifyListeners(K key, V value, String action) {
-        listeners.forEach(l -> notifyListener(l, key, value, action));
+        var it = listeners.iterator();
+        while (it.hasNext()) {
+            Optional.ofNullable(it.next().get())
+                    .ifPresentOrElse(l -> notifyListener(l, key, value, action), it::remove);
+        }
     }
 
     private void notifyListener(HwListener<K, V> listener, K key, V value, String action) {
