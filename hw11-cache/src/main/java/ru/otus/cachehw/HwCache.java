8c1951f3a791981e0bb83fb45842935816f7b9e1
lakidemon <forregistrationsmail@mail.ru>
2020-07-02 03:01:37 +0700

    Revert "[HW11] Nullability markers"
    This reverts commit 05d453da

--- hw11-cache/src/main/java/ru/otus/cachehw/HwCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/HwCache.java
@@ -1,20 +1,16 @@
 package ru.otus.cachehw;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 /**
  * @author sergey
  * created on 14.12.18.
  */
 public interface HwCache<K, V> {
 
-    void put(@Nonnull K key, V value);
+    void put(K key, V value);
 
-    void remove(@Nonnull K key);
+    void remove(K key);
 
-    @Nullable
-    V get(@Nonnull K key);
+    V get(K key);
 
     void addListener(HwListener<K, V> listener);
 
--- hw11-cache/src/main/java/ru/otus/cachehw/HwListener.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/HwListener.java
@@ -1,8 +1,5 @@
 package ru.otus.cachehw;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 /**
  * @author sergey
  * created on 14.12.18.
@@ -12,5 +9,5 @@ public interface HwListener<K, V> {
     public static final String PUT = "PUT";
     public static final String AUTOREMOVE = "AUTOREMOVE";
 
-    void notify(@Nonnull K key, @Nullable V value, String action);
+    void notify(K key, V value, String action);
 }
