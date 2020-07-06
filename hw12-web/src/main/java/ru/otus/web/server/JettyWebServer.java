b7a9988308de1a7767d39811bc8d01b9d8a06f94
lakidemon <forregistrationsmail@mail.ru>
2020-07-07 00:26:22 +0700

    [HW12] Setup server via guice

--- hw12-web/src/main/java/ru/otus/web/server/JettyWebServer.java
+++ hw12-web/src/main/java/ru/otus/web/server/JettyWebServer.java
@@ -1,18 +1,18 @@
 package ru.otus.web.server;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceFilter;
 import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import ru.otus.core.dao.UserDao;
 import ru.otus.core.service.DBServiceUser;
 import ru.otus.core.service.DbServiceUserImpl;
+import ru.otus.core.sessionmanager.SessionManager;
 import ru.otus.hibernate.dao.UserDaoHibernate;
 import ru.otus.hibernate.sessionmanager.SessionManagerHibernate;
 import ru.otus.web.auth.AuthService;
@@ -20,18 +20,22 @@ import ru.otus.web.auth.InMemoryAuthService;
 import ru.otus.web.filter.CharsetFilter;
 import ru.otus.web.filter.LoginFilter;
 import ru.otus.web.servlet.LoginServlet;
+import ru.otus.web.servlet.LogoutServlet;
 import ru.otus.web.servlet.UsersServlet;
 import ru.otus.web.template.FreeMarkerTemplateProvider;
 import ru.otus.web.template.TemplateProvider;
 import ru.otus.web.utils.FileSystemHelper;
 
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
-public class JettyWebServer implements WebServer {
+public class JettyWebServer extends ServletModule implements WebServer {
     private final int port;
-    private Server server;
-    private Injector injector;
+    protected Server server;
+    protected Injector injector;
 
     @Override
     public void init() {
@@ -39,12 +43,23 @@ public class JettyWebServer implements WebServer {
 
         var context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-        setupBeans(context);
-        addServlets(context);
-        addFilters(context);
-        setupStaticContent(context);
+        setupGuice(context);
+        configureDependencies();
 
         server.setHandler(context);
+    }
+
+    protected void configureDependencies() {
+        injector.getInstance(TemplateProvider.class).setup("/templates/");
+        injector.getInstance(AuthService.class).addUser("test", "pass");
+    }
+
+    @Override
+    protected void configureServlets() {
+        setupBindings();
+        addServlets();
+        addFilters();
+        setupStaticContent();
     }
 
     public static void main(String[] args) throws Exception {
@@ -53,44 +68,38 @@ public class JettyWebServer implements WebServer {
         server.start();
     }
 
-    private void setupBeans(ServletContextHandler context) {
-        injector = Guice.createInjector(new BasicModule());
-        context.addEventListener(new GuiceServletContextListener() {
-            @Override
-            protected Injector getInjector() {
-                return injector;
-            }
-        });
+    private void setupGuice(ServletContextHandler context) {
+        injector = Guice.createInjector(this);
+        context.addEventListener(new InternalGuiceServletContextListener());
         context.addFilter(GuiceFilter.class, "/*", null);
     }
 
-    protected void addFilters(ServletContextHandler context) {
-        context.addFilter(CharsetFilter.class, "/**", null);
-        context.addFilter(LoginFilter.class, "/*", null);
+    protected void setupBindings() {
+        bind(AuthService.class).to(InMemoryAuthService.class);
+        bind(TemplateProvider.class).to(FreeMarkerTemplateProvider.class);
+        bind(SessionManager.class).to(SessionManagerHibernate.class);
+        bind(EntityManagerFactory.class).toInstance(Persistence.createEntityManagerFactory("otus-hibernate"));
+        bind(UserDao.class).to(UserDaoHibernate.class);
+        bind(DBServiceUser.class).to(DbServiceUserImpl.class);
     }
 
-    protected void addServlets(ServletContextHandler context) {
-        context.addServlet(LoginServlet.class, "/login");
-        context.addServlet(UsersServlet.class, "/");
+    protected void addFilters() {
+        filter("/*").through(CharsetFilter.class);
+        filter("/*").through(LoginFilter.class, Map.of("excludedPaths", "/static/*"));
     }
 
-    protected void setupStaticContent(ServletContextHandler context) {
-        var servletHolder = new ServletHolder(DefaultServlet.class);
-        servletHolder.setInitParameter("", "true");
-        servletHolder.setInitParameter("resourceBase",
-                FileSystemHelper.localFileNameOrResourceNameToFullPath("static"));
-        context.addServlet(servletHolder, "/static/*");
+    protected void addServlets() {
+        serve("/login").with(LoginServlet.class);
+        serve("/logout").with(LogoutServlet.class);
+        serve("/").with(UsersServlet.class);
     }
 
-    protected class BasicModule extends AbstractModule {
-        @Override
-        protected void configure() {
-            bind(AuthService.class).to(InMemoryAuthService.class);
-            bind(TemplateProvider.class).to(FreeMarkerTemplateProvider.class);
-            bind(UserDao.class).toInstance(new UserDaoHibernate(
-                    new SessionManagerHibernate(Persistence.createEntityManagerFactory("otus-hibernate"))));
-            bind(DBServiceUser.class).to(DbServiceUserImpl.class);
-        }
+    protected void setupStaticContent() {
+        var initParams = new HashMap<String, String>();
+        initParams.put("pathInfoOnly", "true");
+        initParams.put("dirAllowed", "false");
+        initParams.put("resourceBase", FileSystemHelper.localFileNameOrResourceNameToFullPath("static"));
+        serve("/static/*").with(new DefaultServlet(), initParams);
     }
 
     @Override
@@ -104,4 +113,11 @@ public class JettyWebServer implements WebServer {
         server.stop();
     }
 
+    private class InternalGuiceServletContextListener extends GuiceServletContextListener {
+
+        @Override
+        protected Injector getInjector() {
+            return injector;
+        }
+    }
 }
