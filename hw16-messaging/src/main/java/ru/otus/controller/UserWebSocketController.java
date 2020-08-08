01433e7478422c570751e254357a4aa0f49a503e
lakidemon <forregistrationsmail@mail.ru>
2020-08-09 01:20:30 +0700

    [HW16] WebSocket controller

--- hw16-messaging/src/main/java/ru/otus/controller/UserWebSocketController.java
+++ hw16-messaging/src/main/java/ru/otus/controller/UserWebSocketController.java
@@ -1,20 +1,56 @@
 package ru.otus.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import ru.otus.UserMessageType;
+import ru.otus.config.AppConfig;
+import ru.otus.dto.UserDto;
+import ru.otus.dto.UserListDto;
+import ru.otus.messagesystem.client.MsClient;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class UserWebSocketController {
-    private final SimpMessagingTemplate template;
+    @Qualifier("frontendClient")
+    private final MsClient client;
+    private final SimpMessagingTemplate simp;
 
-    @MessageMapping("/hello")
-    public void helloWorld(String message, SimpMessageHeaderAccessor headers) {
-        System.out.println("Got it: " + message + " from " + headers.getUser().getName());
-        template.convertAndSend("/topic/test", "Response");
+    @MessageMapping("/get")
+    public void getAllUsers(SimpMessageHeaderAccessor headers) {
+        var remoteClient = headers.getUser().getName();
+        var msg = client.produceMessage(AppConfig.DB_CLIENTNAME, null, UserMessageType.GET_ALL,
+                (UserListDto data) -> simp.convertAndSendToUser(remoteClient, "/topic/userlist", data.getUsers()));
+        client.sendMessage(msg);
     }
 
+    @MessageMapping("/get.{id}")
+    public void getUser(@DestinationVariable int id, SimpMessageHeaderAccessor headers) {
+        requestUser(headers.getUser().getName(), id);
+    }
+
+    @MessageMapping("/get.random")
+    public void getRandomUser(SimpMessageHeaderAccessor headers) {
+        requestUser(headers.getUser().getName(), -1);
+    }
+
+    private void requestUser(String by, long id) {
+        var msg = client.produceMessage(AppConfig.DB_CLIENTNAME, UserDto.id(id), UserMessageType.GET,
+                (UserDto data) -> simp.convertAndSendToUser(by, "/topic/requesteduser", data));
+        client.sendMessage(msg);
+    }
+
+    @MessageMapping("/add")
+    public void addUser(@Payload UserDto user) {
+        var msg = client.produceMessage(AppConfig.DB_CLIENTNAME, user, UserMessageType.ADD,
+                dto -> simp.convertAndSend("/topic/newuser", dto));
+        client.sendMessage(msg);
+    }
 }
