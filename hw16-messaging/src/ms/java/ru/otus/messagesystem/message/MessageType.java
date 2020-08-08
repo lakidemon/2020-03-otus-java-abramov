183d67027fd97e2bf0861636516068421045f68e
lakidemon <forregistrationsmail@mail.ru>
2020-08-09 01:17:02 +0700

    [HW16] Make MessageType interface

--- hw16-messaging/src/ms/java/ru/otus/messagesystem/message/MessageType.java
+++ hw16-messaging/src/ms/java/ru/otus/messagesystem/message/MessageType.java
@@ -1,15 +1,5 @@
 package ru.otus.messagesystem.message;
 
-public enum MessageType {
-    USER_DATA("UserData");
-
-    private final String name;
-
-    MessageType(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+public interface MessageType {
+    String getName();
 }
